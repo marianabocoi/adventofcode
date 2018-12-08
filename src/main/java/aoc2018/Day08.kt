a934ec1c19ed312c9177da2c0c22cf83484d1e4e
Mariana <mariana.bocoi@gmail.com>
2018-12-08 07:57:52 +0100

    2018 Day 08: simpler not to buind the nodes

--- src/main/java/aoc2018/Day08.kt
+++ src/main/java/aoc2018/Day08.kt
@@ -3,19 +3,71 @@ package aoc2018
 import java.io.File
 
 object Day08 {
-    fun part1(input: List<String>): Int {
-        return 1
+    fun part1(input: List<Int>): Int {
+        return getMetadataSum(input).second
     }
 
-    fun part2(input: List<String>): Int {
-        return 2
+    private fun getMetadataSum(input: List<Int>, index: Int = 0): Pair<Int, Int> {
+
+        val childrenNumber = input[index]
+        val metadataNumber = input[index + 1]
+        var offset = index + 2
+        var sum = 0
+        for (i in 0 until childrenNumber) {
+            val (childOffset, partialSum) = getMetadataSum(input, offset)
+            offset = childOffset
+            sum += partialSum
+        }
+        for (i in offset until offset + metadataNumber)
+            sum += input[i]
+        offset += metadataNumber
+        return (offset to sum)
+    }
+
+    fun part2(input: List<Int>): Int {
+        return getChildrenSum(input).second
+    }
+
+    private fun getChildrenSum(input: List<Int>, index: Int = 0): Pair<Int, Int> {
+
+        val childrenNumber = input[index]
+        val metadataNumber = input[index + 1]
+        var offset = index + 2
+        var sum = 0
+        val childrenValues = arrayOfNulls<Int>(childrenNumber)
+        for (i in 0 until childrenNumber) {
+            val (childOffset, partialSum) = getChildrenSum(input, offset)
+            offset = childOffset
+            childrenValues[i] = partialSum
+        }
+        if (childrenNumber == 0) {
+            for (i in offset until offset + metadataNumber)
+                sum += input[i]
+        } else {
+            val meradata = IntArray(metadataNumber) { input[it + offset] - 1 }
+            val childSum = meradata.filter { it < childrenValues.size }
+                .map { childrenValues[it] }.filterNotNull().sum()
+            sum = childSum
+        }
+        offset += metadataNumber
+        return (offset to sum)
     }
 }
+
+/*
+0 1 2 3 4  5  6  7 8 9 0  1 2 3 4 5
+2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+A----------------------------------
+    B----------- C-----------
+                     D-----
+ */
 
 fun main(args: Array<String>) {
 
     val imputFile = Day01::class.java.classLoader.getResource("aoc2018/day08")
-    val input = File(imputFile.path).readLines()
+    val input = File(imputFile.path).readLines()[0].split("""\s""".toRegex()).map { it.toInt() }
     println("Part1: " + Day08.part1(input))
     println("Part2: " + Day08.part2(input))
 }
+
+data class Node08(val id: Int, val children: List<Node>, val metadata: List<Int>)
--- src/test/java/aoc2018/Day08Test.kt
+++ src/test/java/aoc2018/Day08Test.kt
@@ -7,13 +7,13 @@ class Day08Test {
 
     @Test
     fun part1() {
-        val input = " /n ".split("\n")
-        assertThat(Day07.part1(input)).isEqualTo(1)
+        val input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split(" ").map { it.toInt() }
+        assertThat(Day08.part1(input)).isEqualTo(138)
     }
 
     @Test
     fun part2() {
-        val input = " /n ".split("\n")
-        assertThat(Day07.part2(input)).isEqualTo(2)
+        val input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split(" ").map { it.toInt() }
+        assertThat(Day08.part2(input)).isEqualTo(2)
     }
 }
