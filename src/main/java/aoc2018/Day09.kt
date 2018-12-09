8b80e170e718d19f3a36e66427fe05fefbd41d8d
Mariana <mariana.bocoi@gmail.com>
2018-12-09 13:04:20 +0100

    2018 Day 09: part 2 is an optimisation with double linked lists

--- src/main/java/aoc2018/Day09.kt
+++ src/main/java/aoc2018/Day09.kt
@@ -3,12 +3,67 @@ package aoc2018
 import java.io.File
 
 object Day09 {
-    fun part1(input: List<String>): Int {
-        return 1
+    fun part1(players: Int, points: Int): Int {
+        val circle = mutableListOf<Int>(0, 1)
+        val scores = IntArray(players) { 0 }
+        var currentPosition = 1
+        //   printCircle(circle, currentPosition, 2, players)
+        for (i in 2..points) {
+            if (i % 23 == 0) {
+                currentPosition = (currentPosition - 7) % circle.size
+                if (currentPosition < 0) currentPosition += circle.size
+                scores[i % players] += i + circle.removeAt(currentPosition)
+            } else {
+                currentPosition = (currentPosition + 2) % (circle.size)
+                circle.add(currentPosition, i)
+            }
+            //         printCircle(circle, currentPosition, i, players)
+        }
+        scores.forEachIndexed { i, s -> println("[$i] $s") }
+        var player = points % players - 2
+        if (player < 0) player += players
+        println("Current player: $player")
+        return scores.max()!!
     }
 
-    fun part2(input: List<String>): Int {
-        return 2
+    private fun printCircle(
+        circle: MutableList<Int>,
+        current: Int,
+        index: Int,
+        players: Int
+    ) {
+        val id = index % players + 1
+        print("[$id]\t")
+        for (i in 0 until circle.size) {
+            if (i == current) {
+                print("(${circle[i]})\t")
+            } else {
+                print("${circle[i]}\t")
+            }
+        }
+        println()
+    }
+
+    fun part2(players: Int, points: Long): Long {
+        var root = Node09(0)
+        root.left = root
+        root.right = root
+
+        val scores = LongArray(players) { 0L }
+        for (i in 1L..points) {
+            if (i % 23 == 0L) {
+                val oldNode = root.removeSpecial()
+                scores[(i % players).toInt()] += i + oldNode.value
+                root = oldNode.right!!
+            } else {
+                root = root.add(i)
+            }
+        }
+        scores.forEachIndexed { i, s -> println("[$i] $s") }
+        var player = points % players - 2
+        if (player < 0) player += players
+        println("Current player: $player")
+        return scores.max()!!
     }
 }
 
@@ -16,6 +71,24 @@ fun main(args: Array<String>) {
 
     val imputFile = Day01::class.java.classLoader.getResource("aoc2018/day09")
     val input = File(imputFile.path).readLines()
-    println("Part1: " + Day09.part1(input))
-    println("Part2: " + Day09.part2(input))
+    println("Part1: " + Day09.part1(403, 71920))
+    println("Part2: " + Day09.part2(403, 7192000))// not 439089
+}
+
+data class Node09(val value: Long, var left: Node09? = null, var right: Node09? = null) {
+    fun add(v: Long): Node09 {
+        val newNode = Node09(v)
+        newNode.left = this.right
+        newNode.right = this.right!!.right
+        this.right!!.right!!.left = newNode
+        this.right!!.right = newNode
+        return newNode
+    }
+
+    fun removeSpecial(): Node09 {
+        val replaceMe = this.left!!.left!!.left!!.left!!.left!!.left!!.left!!
+        replaceMe.left!!.right = replaceMe.right
+        replaceMe.right!!.left = replaceMe.left
+        return replaceMe
+    }
 }
--- src/test/java/aoc2018/Day09Test.kt
+++ src/test/java/aoc2018/Day09Test.kt
@@ -6,14 +6,63 @@ import org.junit.Test
 class Day09Test {
 
     @Test
-    fun part1() {
-        val input = " /n ".split("\n")
-        assertThat(Day07.part1(input)).isEqualTo(1)
+    fun part101() {
+        assertThat(Day09.part1(9, 25)).isEqualTo(32)
     }
 
     @Test
-    fun part2() {
-        val input = " /n ".split("\n")
-//        assertThat(Day07.part2(input),2).isEqualTo(2)
+    fun part102() {
+        assertThat(Day09.part1(10, 1618)).isEqualTo(8317)
+    }
+
+    @Test
+    fun part103() {
+        assertThat(Day09.part1(13, 7999)).isEqualTo(146373)
+    }
+
+    @Test
+    fun part104() {
+        assertThat(Day09.part1(17, 1104)).isEqualTo(2764)
+    }
+
+    @Test
+    fun part105() {
+        assertThat(Day09.part1(21, 6111)).isEqualTo(54718)
+    }
+
+    @Test
+    fun part106() {
+        assertThat(Day09.part1(30, 5807)).isEqualTo(37305)
+    }
+
+
+    @Test
+    fun part201() {
+        assertThat(Day09.part2(9, 25)).isEqualTo(32)
+    }
+
+    @Test
+    fun part202() {
+        assertThat(Day09.part2(10, 1618)).isEqualTo(8317)
+    }
+
+    @Test
+    fun part203() {
+        assertThat(Day09.part2(13, 7999)).isEqualTo(146373)
+    }
+
+    @Test
+    fun part204() {
+        assertThat(Day09.part2(17, 1104)).isEqualTo(2764)
+    }
+
+    @Test
+    fun part205() {
+        assertThat(Day09.part2(21, 6111)).isEqualTo(54718)
+    }
+
+    @Test
+    fun part206() {
+        assertThat(Day09.part2(30, 5807)).isEqualTo(37305)
     }
 }
