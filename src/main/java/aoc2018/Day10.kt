f50a1f42b2a8f18e5579299e8b1b2b30def66c7e
Mariana <mariana.bocoi@gmail.com>
2018-12-10 07:29:29 +0100

    2018 Day 10: <3

--- src/main/java/aoc2018/Day10.kt
+++ src/main/java/aoc2018/Day10.kt
@@ -1,9 +1,50 @@
 package aoc2018
 
 import java.io.File
+import java.io.FileOutputStream
 
 object Day10 {
-    fun part1(input: List<String>): Int {
+
+    val guardRegex = """position=<\s*(\S*),\s*(\S*)> velocity=<\s*(\S*),\s*(\S*)>""".toRegex()
+    fun parsePixels(line: String): Pixel? {
+        guardRegex.matchEntire(line)?.groups?.let { input ->
+            val details = input.drop(1).mapNotNull { i -> i?.value?.toInt() }
+            if (details.isNotEmpty()) {
+                return Pixel(details[0], details[1], details[2], details[3])
+            }
+        }
+        return null
+    }
+
+    fun part1(input: List<String>, seconds: Int = 5): Int {
+        val pixels = input.mapNotNull { parsePixels(it) }
+
+        for (s in 0..seconds) {
+            val movedPixels = pixels.map { (x, y, vx, vy) -> Pixel(x + s * vx, y + s * vy, vx, vy) }
+            val maxX = movedPixels.maxBy { p -> p.x }!!.x
+            val maxY = movedPixels.maxBy { p -> p.y }!!.y
+            val minX = movedPixels.minBy { p -> p.x }!!.x
+            val minY = movedPixels.minBy { p -> p.y }!!.y
+            if (maxX - minX < 100 && maxY - minY < 100) {
+                val sorted = movedPixels.groupBy { it.y }.toSortedMap()
+                val line = StringBuffer()
+                line.append("Text for second: $s\n")
+                for (i in minY..maxY) {
+                    val xValues = sorted[i]?.associate { p -> p.x to p.y } ?: emptyMap()
+                    for (j in minX..maxX) {
+                        if (xValues.containsKey(j)) {
+                            line.append("#")
+                        } else {
+                            line.append(".")
+                        }
+                    }
+                    line.append("\n")
+                }
+                FileOutputStream("yay.txt", true).bufferedWriter().use { writer ->
+                    writer.append(line.toString())
+                }
+            }
+        }
         return 1
     }
 
@@ -14,8 +55,10 @@ object Day10 {
 
 fun main(args: Array<String>) {
 
-    val inputFile = Day01::class.java.classLoader.getResource("aoc2018/day10")
+    val inputFile = Day10::class.java.classLoader.getResource("aoc2018/day10")
     val input = File(inputFile.path).readLines()
-    println("Part1: " + Day10.part1(input))
+    println("Part1: " + Day10.part1(input, 1000000))
     println("Part2: " + Day10.part2(input))
 }
+
+data class Pixel(val x: Int, val y: Int, val vx: Int, val vy: Int)
--- src/test/java/aoc2018/Day10Test.kt
+++ src/test/java/aoc2018/Day10Test.kt
@@ -7,8 +7,38 @@ class Day10Test {
 
     @Test
     fun part1() {
-        val input = " /n ".split("\n")
-        assertThat(Day10.part1(input)).isEqualTo(1)
+        val input = "position=< 9,  1> velocity=< 0,  2>\n" +
+            "position=< 7,  0> velocity=<-1,  0>\n" +
+            "position=< 3, -2> velocity=<-1,  1>\n" +
+            "position=< 6, 10> velocity=<-2, -1>\n" +
+            "position=< 2, -4> velocity=< 2,  2>\n" +
+            "position=<-6, 10> velocity=< 2, -2>\n" +
+            "position=< 1,  8> velocity=< 1, -1>\n" +
+            "position=< 1,  7> velocity=< 1,  0>\n" +
+            "position=<-3, 11> velocity=< 1, -2>\n" +
+            "position=< 7,  6> velocity=<-1, -1>\n" +
+            "position=<-2,  3> velocity=< 1,  0>\n" +
+            "position=<-4,  3> velocity=< 2,  0>\n" +
+            "position=<10, -3> velocity=<-1,  1>\n" +
+            "position=< 5, 11> velocity=< 1, -2>\n" +
+            "position=< 4,  7> velocity=< 0, -1>\n" +
+            "position=< 8, -2> velocity=< 0,  1>\n" +
+            "position=<15,  0> velocity=<-2,  0>\n" +
+            "position=< 1,  6> velocity=< 1,  0>\n" +
+            "position=< 8,  9> velocity=< 0, -1>\n" +
+            "position=< 3,  3> velocity=<-1,  1>\n" +
+            "position=< 0,  5> velocity=< 0, -1>\n" +
+            "position=<-2,  2> velocity=< 2,  0>\n" +
+            "position=< 5, -2> velocity=< 1,  2>\n" +
+            "position=< 1,  4> velocity=< 2,  1>\n" +
+            "position=<-2,  7> velocity=< 2, -2>\n" +
+            "position=< 3,  6> velocity=<-1, -1>\n" +
+            "position=< 5,  0> velocity=< 1,  0>\n" +
+            "position=<-6,  0> velocity=< 2,  0>\n" +
+            "position=< 5,  9> velocity=< 1, -2>\n" +
+            "position=<14,  7> velocity=<-2,  0>\n" +
+            "position=<-3,  6> velocity=< 2, -1>"
+        assertThat(Day10.part1(input.split("\n"))).isEqualTo(1)
     }
 
     @Test
