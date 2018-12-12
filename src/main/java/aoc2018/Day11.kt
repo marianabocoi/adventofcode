1c4ef7fec34bd08391fb3eff49917341609c1f08
Mariana <mariana.bocoi@gmail.com>
2018-12-12 06:07:36 +0100

    2018 Day 11: Cache for the win

--- src/main/java/aoc2018/Day11.kt
+++ src/main/java/aoc2018/Day11.kt
@@ -3,19 +3,127 @@ package aoc2018
 import java.io.File
 
 object Day11 {
-    fun part1(input: List<String>): Int {
-        return 1
+    private const val width = 300
+    private const val height = 300
+    private val cache = mutableMapOf<Long, Array<Array<Long?>>>()
+    private val sumCache = mutableMapOf<Pair<Int, Long>, Array<Array<Long?>>>()
+
+    fun part1(input: Long, gridSize: Int = 3): Cell {
+        val batterySum: Array<Array<Long?>> = cachedSums(input, gridSize)
+
+        var maxX = -1
+        var maxY = -1
+        var fuel = -10000L
+        for (i in 0 until width - gridSize) {
+            for (j in 0 until height - gridSize) {
+                if (batterySum[i][j]!! > fuel) {
+                    maxX = i + 1
+                    maxY = j + 1
+                    fuel = batterySum[i][j]!!
+                }
+            }
+        }
+        return Cell(maxX, maxY, gridSize, fuel)
     }
 
-    fun part2(input: List<String>): Int {
-        return 2
+    fun cachedSums(
+        input: Long,
+        gridSize: Int
+    ): Array<Array<Long?>> {
+        val battery = getFuelCellValues(input)
+        val batterySum: Array<Array<Long?>>
+        if (gridSize == 1) {
+            batterySum = getFuelCellValues(input).clone()
+        } else {
+            if (!sumCache.containsKey(gridSize - 1 to input)) {
+                cachedSums(input, gridSize - 1)
+            }
+            batterySum = Array(width) { arrayOfNulls<Long>(height) }
+            val previousSum = sumCache[gridSize - 1 to input]!!.clone()
+            for (i in 0..width - gridSize) {
+                for (j in 0..height - gridSize) {
+                    val sumVerticals = IntArray(gridSize - 1) { it }
+                        .map { battery[i + gridSize - 1][j + it]!! + battery[i + it][j + gridSize - 1]!! }
+                        .sum()
+                    batterySum[i][j] = previousSum[i][j]!! +
+                        battery[i + gridSize - 1][j + gridSize - 1]!! + sumVerticals
+                }
+            }
+        }
+        sumCache[gridSize to input] = batterySum
+        return batterySum.clone()
+    }
+
+    fun naiveSums(
+        input: Long,
+        gridSize: Int
+    ): Array<Array<Long?>> {
+        val battery = getFuelCellValues(input)
+        val batterySum: Array<Array<Long?>> = Array(width) { arrayOfNulls<Long>(height) }
+        for (i in 0..width - gridSize) {
+            for (j in 0..height - gridSize) {
+                batterySum[i][j] = 0
+                for (k in 0 until gridSize) {
+                    for (l in 0 until gridSize) {
+                        batterySum[i][j] = batterySum[i][j]!! + battery[i + k][j + l]!!
+                    }
+                }
+            }
+        }
+        return batterySum
+    }
+
+    private fun getFuelCellValues(
+        gridSerialNumber: Long
+    ): Array<Array<Long?>> {
+        if (cache.containsKey(gridSerialNumber)) {
+            return cache[gridSerialNumber]!!
+        }
+
+        val battery = Array(width) { arrayOfNulls<Long>(height) }
+        for (i in 0 until width) {
+            for (j in 0 until height) {
+                battery[i][j] = getFuelLevel((i + 1).toLong(), (j + 1).toLong(), gridSerialNumber)
+            }
+        }
+        cache[gridSerialNumber] = battery.clone()
+        return battery
+    }
+
+    fun getFuelLevel(x: Long, y: Long, gridSerialNumber: Long): Long {
+        if (x <= 300 && y <= 300) {
+            val rackId = x + 10
+            val initialPowerLevel = rackId * y
+            val plusGridSerialNumber = initialPowerLevel + gridSerialNumber
+            val multipliedWithRackId = plusGridSerialNumber * rackId
+            if (multipliedWithRackId < 100) {
+                return 0
+            }
+            return (multipliedWithRackId % 1000) / 100 - 5
+        }
+        return -1000
+    }
+
+    fun part2(input: Long): Cell {
+        return IntArray(300) { it + 1 }.map { gridSize ->
+            part1(
+                input,
+                gridSize
+            )
+        }.maxBy { it.value }!!
+    }
+}
+
+data class Cell(val x: Int, val y: Int, val gridSize: Int, val value: Long) {
+    override fun toString(): String {
+        return "$x,$y,$gridSize,$value"
     }
 }
 
 fun main(args: Array<String>) {
 
     val inputFile = Day11::class.java.classLoader.getResource("aoc2018/day11")
-    val input = File(inputFile.path).readLines()
+    val input = File(inputFile.path).readLines()[0].toLong()
     println("Part1: " + Day11.part1(input))
     println("Part2: " + Day11.part2(input))
 }
--- src/test/java/aoc2018/Day11Test.kt
+++ src/test/java/aoc2018/Day11Test.kt
@@ -7,13 +7,53 @@ class Day11Test {
 
     @Test
     fun part1() {
-        val input = " /n ".split("\n")
-        assertThat(Day11.part1(input)).isEqualTo(1)
+        assertThat(Day11.getFuelLevel(3, 5, 8)).isEqualTo(4)
+        assertThat(Day11.getFuelLevel(122, 79, 57)).isEqualTo(-5)
+        assertThat(Day11.getFuelLevel(217, 196, 39)).isEqualTo(0)
+        assertThat(Day11.getFuelLevel(101, 153, 71)).isEqualTo(4)
+
+        assertThat(Day11.part1(18L).toString()).isEqualTo("33,45,3,29")
+        assertThat(Day11.part1(42).toString()).isEqualTo("21,61,3,30")
     }
 
     @Test
     fun part2() {
-        val input = " /n ".split("\n")
-        assertThat(Day11.part2(input)).isEqualTo(2)
+        assertThat(Day11.part2(18L).toString()).isEqualTo("90,269,16,113")
+        assertThat(Day11.part2(42L).toString()).isEqualTo("232,251,12,119")
     }
+
+    @Test
+    fun cacheTest() {
+        var cached = Day11.cachedSums(18L, 1)
+        var notCached = Day11.naiveSums(18L, 1)
+        println("pass 1")
+        printSelection(cached)
+        printSelection(notCached)
+        assertThat(cached).isEqualTo(Day11.naiveSums(18L, 1))
+
+        cached = Day11.cachedSums(18L, 2)
+        notCached = Day11.naiveSums(18L, 2)
+        println("pass 2")
+        printSelection(cached)
+        printSelection(notCached)
+        assertThat(cached).isEqualTo(notCached)
+
+        cached = Day11.cachedSums(18L, 3)
+        notCached = Day11.naiveSums(18L, 3)
+        println("pass 2")
+        printSelection(cached)
+        printSelection(notCached)
+        assertThat(cached).isEqualTo(notCached)
+    }
+
+    private fun printSelection(cached: Array<Array<Long?>>) {
+        for (i in 0 until 10) {
+            for (j in 0 until 10) {
+                print(cached[i][j]!!.toString() + "\t\t")
+            }
+            println()
+        }
+        println()
+    }
+
 }
