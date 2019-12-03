3847bcd2d39722ccbc22de815f587a7e87deb368
Mariana <mariana.bocoi@gmail.com>
2019-12-03 19:01:12 +0100

    refactor

--- aoc2019/src/main/kotlin/Day01.kt
+++ aoc2019/src/main/kotlin/Day01.kt
@@ -1,20 +1,22 @@
 import kotlin.math.floor
 
-fun calculateFuel(mass: Double) = (floor(mass.toDouble() / 3) - 2)
+object Day01 {
+    fun calculateFuel(mass: Double) = (floor(mass.toDouble() / 3) - 2)
 
-fun calculateFuelRecursively(mass: Double): Double {
-    val fuel = calculateFuel(mass)
-    return if (fuel > 0) fuel + calculateFuelRecursively(fuel) else 0.0
+    fun calculateFuelRecursively(mass: Double): Double {
+        val fuel = calculateFuel(mass)
+        return if (fuel > 0) fuel + calculateFuelRecursively(fuel) else 0.0
+    }
+
+    fun part1(input: List<Double>) = input.map { calculateFuel(it) }.sum()
+
+    fun part2(input: List<Double>) = input.map { calculateFuelRecursively(it) }.sum()
 }
 
-fun part1(input: List<Double>) = input.map { calculateFuel(it) }.sum()
-
-fun part2(input: List<Double>): Double = input.map { calculateFuelRecursively(it) }.sum()
-
-fun main(args: Array<String>) {
+fun main() {
     val input = Utils.getInputAsDoubleList("input_day01.txt")
-    val part1 = part1(input)
+    val part1 = Day01.part1(input)
     println("Day 1 part 1: $part1")
-    val part2 = part2(input)
+    val part2 = Day01.part2(input)
     println("Day 1 part 2: $part2")
 }
--- aoc2019/src/main/kotlin/Utils.kt
+++ aoc2019/src/main/kotlin/Utils.kt
@@ -1,9 +1,11 @@
 class Utils {
     companion object {
         fun getInput(filename: String) = Utils::class.java.getResource(filename).readText()
-        fun getInputAsList(filename: String): List<String> = getInput(filename).split("\n")
-        fun getInputAsIntList(filename: String): List<Int> =
-            getInputAsList(filename).map { it.toInt() }
+        fun getInputAsList(filename: String, delim: String = "\n"): List<String> =
+            getInput(filename).split(delim)
+
+        fun getInputAsIntList(filename: String, delim: String = "\n"): List<Int> =
+            getInputAsList(filename, delim).map { it.toInt() }
 
         fun getInputAsDoubleList(filename: String): List<Double> =
             getInputAsList(filename).map { it.toDouble() }
